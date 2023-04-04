4ff343135c28c70ae456a38d221f60485c887760
Kellymuterian <77204015+Kellymuterian@users.noreply.github.com>
2023-04-04 22:45:05 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\MemberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ToDoController;
+use App\Http\Controllers\Api\MemberController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -15,3 +16,10 @@ Route::get('member/{id}', [MemberController::class, 'show']);
 Route::get('member/{id}/edit', [MemberController::class, 'edit']);
 Route::put('member/{id}/edit', [MemberController::class, 'update']);
 Route::delete('member/{id}/delete', [MemberController::class, 'destroy']);
+
+Route::get('todo', [ToDoController::class, 'papi']);
+Route::post('todo', [ToDoController::class, 'store']);
+Route::get('todo/{id}', [ToDoController::class, 'show']);
+Route::get('todo/{id}/edit', [ToDoController::class, 'edit']);
+Route::put('todo/{id}/edit', [ToDoController::class, 'update']);
+Route::delete('todo/{id}/delete', [ToDoController::class, 'destroy']);
